aabcac1792de633aeab8a0dd4107fc91c262d240
gribdesbois <gribdesbois@gmail.com>
2022-04-03 21:46:17 +0200

    action

--- vite-project/src/stores/CounterStore/CounterStore.js
+++ vite-project/src/stores/CounterStore/CounterStore.js
@@ -1,8 +1,11 @@
-import { observable, decorate, computed } from 'mobx'
+import { observable, decorate, computed, action } from 'mobx'
 
 class CounterStore {
   @observable counter = 0
   @computed get counterMoreThan100() {
     return this.counter > 100
+  }
+  @action incrementCounter() {
+    return this.counter + 1
   }
 }
