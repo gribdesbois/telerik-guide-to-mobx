db9da31fde26e1797c7695c6b4c7b49e466a1a67
gribdesbois <gribdesbois@gmail.com>
2022-04-03 21:45:12 +0200

    using decorators

--- vite-project/src/stores/CounterStore/CounterStore.js
+++ vite-project/src/stores/CounterStore/CounterStore.js
@@ -1,1 +1,8 @@
-import { observable } from 'mobx'
+import { observable, decorate, computed } from 'mobx'
+
+class CounterStore {
+  @observable counter = 0
+  @computed get counterMoreThan100() {
+    return this.counter > 100
+  }
+}
